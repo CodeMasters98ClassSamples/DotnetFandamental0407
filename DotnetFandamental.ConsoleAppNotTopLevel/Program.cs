7d3cff16ee709808ae6d2102f2080624e7d21c82
Administrator <Administrator@PC-12>
2025-10-08 20:23:18 +0330

    Add project files.

---
+++ DotnetFandamental.ConsoleAppNotTopLevel/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DotnetFandamental.ConsoleAppNotTopLevel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Varibale
+            //Type(نوع) nameOfVaribale = intialValue(مقدار دهی اولیه);
+            string helloWorldText = "Hello, World!";
+            Console.WriteLine(helloWorldText);
+
+            bool isRegisted = true;
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("your name is: " + name);
+
+        }
+        
+    }
+}
