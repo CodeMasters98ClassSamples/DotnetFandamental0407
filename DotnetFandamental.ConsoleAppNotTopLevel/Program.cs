887e93159e8efaffc221563aa8cef1a9021074b8
Administrator <Administrator@PC-12>
2025-10-15 20:21:55 +0330

    Session 03 code

--- DotnetFandamental.ConsoleAppNotTopLevel/Program.cs
+++ DotnetFandamental.ConsoleAppNotTopLevel/Program.cs
@@ -4,18 +4,37 @@
     {
         static void Main(string[] args)
         {
-            //Declare Varibale
-            //Type(نوع) nameOfVaribale = intialValue(مقدار دهی اولیه);
+    
             string helloWorldText = "Hello, World!";
             Console.WriteLine(helloWorldText);
 
-            bool isRegisted = true;
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine() ?? "";
 
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("your name is: " + name);
+            if (string.IsNullOrEmpty(firstName) || firstName.Length <= 2)
+            {
+                Console.WriteLine("Invalid firstName");
+            }
+            else
+            {
+                Console.WriteLine("valid firstName");
+            }
+
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine() ?? "";
+
+            Console.WriteLine($"my name is: {firstName} {lastName}");
+
+            Console.WriteLine("How old are you?");
+            string ageStr = Console.ReadLine() ?? "";
+            int age = int.Parse(ageStr);
+            Console.WriteLine("age: " + age);
+
+            Console.WriteLine("What is your phoneNumber?");
+            string phoneNumber = Console.ReadLine() ?? "";
+            Console.WriteLine("phoneNumber: " + phoneNumber);
 
         }
-        
+
     }
 }
---
+++ DotnetFandamental.ConsoleAppNotTopLevel/Session03.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetFandamental.ConsoleAppNotTopLevel
+{
+    internal class Session03
+    {
+        public void Learning()
+        {
+            //Declare Varibale
+            //Type(نوع) nameOfVaribale = intialValue(مقدار دهی اولیه);
+            string ageStr = "25";
+            int age = int.Parse(ageStr);
+            Convert.ToInt32(ageStr);
+            long ageLong = long.Parse(ageStr);
+            decimal.Parse(ageStr);
+            double.Parse(ageStr);
+
+            ageStr = age.ToString();
+
+            short shortNumber = 5;
+            int intNumber = 15;
+            intNumber = shortNumber;
+
+            shortNumber = (short)intNumber;
+        }
+    }
+}
