013d76bb1c6c46821f10655366a1573c86d3732f
Administrator <Administrator@PC-12>
2025-10-29 20:30:57 +0330

    change on program.cs

--- DotnetFandamental.ConsoleAppNotTopLevel/Program.cs
+++ DotnetFandamental.ConsoleAppNotTopLevel/Program.cs
@@ -4,36 +4,108 @@
     {
         static void Main(string[] args)
         {
-    
-            string helloWorldText = "Hello, World!";
-            Console.WriteLine(helloWorldText);
 
-            Console.WriteLine("What is your first name?");
-            string firstName = Console.ReadLine() ?? "";
-
-            if (string.IsNullOrEmpty(firstName) || firstName.Length <= 2)
+            try
             {
-                Console.WriteLine("Invalid firstName");
+
+                do
+                {
+                    Console.WriteLine("What is your phoneNumber?");
+                    string phoneNumber = Console.ReadLine() ?? "";
+                    phoneNumber = FormatMobileNumber(phoneNumber);
+                    if (IsValidMobileNumber(phoneNumber))
+                        break;
+                    else
+                        Console.WriteLine("Invalid phoneNumber. Please enter valid phoneNumber!");
+                } while (true);
+
+               
+                string helloWorldText = "Hello, World!";
+                Console.WriteLine(helloWorldText);
+
+                Console.WriteLine("What is your first name?");
+                string firstName = "";
+                do
+                {
+                    firstName = Console.ReadLine() ?? "";
+                    if (string.IsNullOrEmpty(firstName) || firstName.Length <= 2)
+                        Console.WriteLine("Invalid firstName. Please enter valid name!");
+                    else
+                        break;
+                } while (true);
+
+                Console.WriteLine("What is your last name?");
+                string lastName = "";
+                while (true)
+                {
+                    lastName = Console.ReadLine() ?? "";
+                    if (string.IsNullOrEmpty(lastName) || lastName.Length <= 2)
+                        Console.WriteLine("Invalid lastName. Please enter valid lastName!");
+                    else
+                        break;
+                }
+
+                Console.WriteLine($"my name is: {firstName} {lastName}");
+
+                Console.WriteLine("How old are you?");
+                string ageStr = "";
+                while (true)
+                {
+                    ageStr = Console.ReadLine() ?? "";
+                    int age = int.Parse(ageStr);
+                    if (age <= 10 || age > 100)
+                        Console.WriteLine("Invalid age. Please enter valid age!");
+                    else
+                        break;
+                }
+
             }
+            catch (Exception ex)
+            {
+                //Business of log
+                throw;
+            }
+
+        }
+
+
+        //Method Overload
+        //DRY -> Dont repeat yourself
+        //static int Sum(int a1, int a2)
+        //{
+        //    int result = a1 + a2;
+        //    return result;
+        //}
+
+        
+
+        static bool IsValidMobileNumber(string phoneNumber)
+        {
+            if (string.IsNullOrEmpty(phoneNumber))
+                return false;
+
+            if (phoneNumber.Length == 11 && phoneNumber.StartsWith('0'))
+                return true;
             else
+                return false;
+        }
+
+        static string FormatMobileNumber(string phoneNumber)
+        {
+            //Reusable Code
+            //9129564205 -> 09129564205
+            if (phoneNumber.Length == 10 && !phoneNumber.StartsWith('0'))
             {
-                Console.WriteLine("valid firstName");
+                phoneNumber = "0" + phoneNumber;
             }
 
-            Console.WriteLine("What is your last name?");
-            string lastName = Console.ReadLine() ?? "";
+            //989129564205 -> 09129564205
+            if (phoneNumber.Length == 12 && phoneNumber.StartsWith("98"))
+            {
+                phoneNumber = phoneNumber.Replace("98", "0");
+            }
 
-            Console.WriteLine($"my name is: {firstName} {lastName}");
-
-            Console.WriteLine("How old are you?");
-            string ageStr = Console.ReadLine() ?? "";
-            int age = int.Parse(ageStr);
-            Console.WriteLine("age: " + age);
-
-            Console.WriteLine("What is your phoneNumber?");
-            string phoneNumber = Console.ReadLine() ?? "";
-            Console.WriteLine("phoneNumber: " + phoneNumber);
-
+            return phoneNumber; 
         }
 
     }
---
+++ DotnetFandamental.ConsoleAppNotTopLevel/Session05.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetFandamental.ConsoleAppNotTopLevel
+{
+    internal class Session05
+    {
+        public static void main()
+        {
+            Console.WriteLine("First number:");
+            string num1Str = Console.ReadLine() ?? "";
+            int num1 = int.Parse(num1Str);
+
+            Console.WriteLine("Second number:");
+            string num2Str = Console.ReadLine() ?? "";
+            int num2 = int.Parse(num2Str);
+
+            int result = Sum(num1, num2);
+
+            Console.WriteLine($"Num1 is : {num1} and num2 is : {num2} and result is : {result}");
+
+        }
+
+        static int Sum(int num1, int num2, int num3 = 0)
+        {
+            int result = num1 + num2 + num3;
+            return result;
+        }
+    }
+}
